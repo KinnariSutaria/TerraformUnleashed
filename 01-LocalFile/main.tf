bbc349ebde31a70456f687b6685b24186eab5ced
kinnarisutaria <kinnarisutaria9@gmail.com>
2023-10-26 21:35:55 +0530

    [Add] | Localfile initial code

--- 01-LocalFile/main.tf
+++ 01-LocalFile/main.tf
@@ -1,4 +1,14 @@
+provider "local" {
+
+}
+
 resource "local_file" "sample_local_file" {
   filename = "sample_local_file.txt"
   content  = "Congratulations! You just created your first localfile"
+
+}
+
+output "file_name" {
+  value = local_file.sample_local_file.filename
+
 }
