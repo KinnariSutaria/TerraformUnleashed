c4ee23adbf67bc7f7ca0095cabdab06184c0f08c
kinnarisutaria <kinnarisutaria9@gmail.com>
2023-10-26 21:00:08 +0530

    [Add] | Localfile assignment code

---
+++ LocalFileAndTime-Assignment/main.tf
@@ -0,0 +1,15 @@
+resource "time_static" "example" {
+
+}
+
+output "current_time" {
+  value = time_static.example.id
+}
+
+/*
+Now, update the main.tf file and add a new local_file resource called time with the following requirements:
+
+filename: time.txt
+content: Time stamp of this file is <id from time_static resource>
+
+*/
